dfb261c3673073498c019dae234ebf8049dbbf86
Savasmetin <savasmetin333@gmail.com>
2022-04-07 14:58:14 +0300

    V3

--- Main.java
+++ Main.java
@@ -3,7 +3,13 @@ class Main {
     return a+b;
 }
 public static void bölme(int a,int b) {
-  System.out.println( a / b);
+  try {
+    int c=0;
+    c=a/b;
+      System.out.println(c);
+  } catch (ArithmeticException e) {
+      System.out.println("Sıfıra bölme hatası oluştu");
+  }
 }
     public static void main(String[] args) {
      
