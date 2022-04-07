7979c12d693db4d3fba17c9621b35b9bae6e817c
Savasmetin <savasmetin333@gmail.com>
2022-04-07 14:56:26 +0300

    V2

--- Main.java
+++ Main.java
@@ -2,6 +2,9 @@ class Main {
   public static int toplama(int a,int b){
     return a+b;
 }
+public static void bölme(int a,int b) {
+  System.out.println( a / b);
+}
     public static void main(String[] args) {
      
     }
